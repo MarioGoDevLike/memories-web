3ed54222b5b7fef813e1a2efaabc6d1a4dc6f282
MarioGoDevLike <marionassar57@gmail.com>
2024-09-05 09:19:12 +0300

    created controller for posts, the model schema and the routes

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
+import PostRoutes from './routes/posts.js';
+
 
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors());
+app.use('/posts', PostRoutes);
+
+
 
 mongoose
   .connect(process.env.CONNECTION_URL)
